83d76c6db942884be46cc996129d7e7dc77b1e54
mohamedali7592salem-eng <mohamedali7592salem@gmail.com>
2026-02-20 14:26:36 +0200

    Create footer.php

---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+</main>
+    <footer>
+        <div class="footer-content">
+            <p>جميع الحقوق محفوظة &copy; <?php echo date('Y'); ?> موقعي</p>
+        </div>
+    </footer>
+</body>
+</html>
